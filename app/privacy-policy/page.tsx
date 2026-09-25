eed524ec28f062958796ff460b9f590035d92caa
One Tech Work <officialonetechwork@gmail.com>
2026-09-25 21:01:07 +0530

    Update page.tsx

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,5 +1,11 @@
 import PrivacyPolicy from "@/app/components/PrivacyPolicy";
+import Footer from "@/app/components/components/Footer"; 
 
 export default function PrivacyPolicyPage() {
-  return <PrivacyPolicy />;
+  return (
+    <>
+      <PrivacyPolicy />
+      <Footer />
+    </>
+  );
 }
