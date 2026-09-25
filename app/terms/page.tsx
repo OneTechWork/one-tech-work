b83a3fd04f5644227bde931f07696c13cb1c6f14
One Tech Work <officialonetechwork@gmail.com>
2026-09-25 21:06:12 +0530

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,5 +1,11 @@
 import TermsOfService from "@/app/components/TermsofService";
+import Footer from "@/app/components/components/Footer";
 
 export default function TermsPage() {
-  return <TermsOfService />;
+  return (
+    <>
+      <TermsOfService />
+      <Footer />
+    </>
+  );
 }
